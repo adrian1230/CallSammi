918b3aba9419160fe1cf10b37051ed0b0e4ec03c
ADRIAN - CHUN CHEONG SO <snfwie2i@gmail.com>
2020-03-09 13:02:10 +0800

    aasd

--- understand/filters.py
+++ understand/filters.py
@@ -4,7 +4,6 @@ from understand.models import Result
 from django_filters import DateFilter, CharFilter
 
 class ResultFilter(django_filters.FilterSet):
-	# date = DateFilter(field_name="date",lookup_expr="gte")
 	title = CharFilter(field_name="title",lookup_expr="icontains")
 
 	class Meta:
